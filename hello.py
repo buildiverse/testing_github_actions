5676562adce6d9bbc75ea1bc5ae727b90342b5b0
Gurnav224 <chaudharyg856@gmail.com>
2025-08-24 08:01:02 +0530

    testing branch update

--- hello.py
+++ hello.py
@@ -1,18 +1,10 @@
 
 
-print('Hello, World!')
+num = int(input("Enter a number: "))
 
-print('welcome to test the ci and cd')
-
-
-print('this is the test branch do not merge it directly to main')
-
-print('how it works in the test branch')
-
-print('add the changes')
-
-print('remove the permissions from the global CI/CD workflow')
-
-print('add the permissions to each job in the CI/CD workflow')
-
-print('now the pr checks should work as expected')
+if num % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
+    
+    
